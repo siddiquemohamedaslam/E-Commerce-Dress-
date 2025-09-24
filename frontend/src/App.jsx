50be4069879075455d63f13d2767ccab5e26fb42
Siddique Mohamed Aslam <aslamsiddique3@gmail.com>
2025-09-24 12:06:03 +0530

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import About from './pages/About'
+import Order from './pages/Order'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vm] md:px-[7vm] lg:px-[9vm] '>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>{/**searching for prodct id */}
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
   )
 }
 
